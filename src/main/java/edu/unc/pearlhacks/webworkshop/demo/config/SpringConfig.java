191139ecee0aa26b4685ad2561f52c398f4787d2
Brenda Cardenas <BCardenas-Patlan@captechconsulting.com>
2020-02-21 14:06:18 -0500

    Removing commented code line

--- src/main/java/edu/unc/pearlhacks/webworkshop/demo/config/SpringConfig.java
+++ src/main/java/edu/unc/pearlhacks/webworkshop/demo/config/SpringConfig.java
@@ -10,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SpringConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    //registry.addMapping("/**").allowedOrigins("http://localhost:4200");
     registry.addMapping("/**")
         .allowedOrigins("*")
         .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
